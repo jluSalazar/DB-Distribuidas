17b0ac688b4b69303773c4283f3cca23b2d00c1b
Jonathan <Jonathan@DESKTOP-8Q2R2ND>
2023-06-25 11:53:50 -0500

    Agregar archivos de proyecto.

---
+++ WinAppBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace WinAppBiblioteca.Logica
+{
+    public class Conexion
+    {
+        private string baseDatos;
+        private static Conexion con = null;
+
+        private Conexion() 
+        {
+            this.baseDatos = "./Biblioteca.db";
+        }
+
+        public SQLiteConnection crearConexion()
+        {
+            SQLiteConnection cadena = new SQLiteConnection();
+            try
+            {
+                cadena.ConnectionString = "Data Source="+this.baseDatos;
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw ex;
+            }
+            return cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (con == null)
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
+    }
+}
