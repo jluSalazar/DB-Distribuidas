89a58c2034ef9a804c190bc56d2c51a75f397820
Marlow Armijo <marlowarmijolopez@gmail.com>
2023-08-28 22:05:59 -0500

    cambio

---
+++ WinAppBiblioteca/Forms/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WinAppBiblioteca.Forms
+{
+    public partial class Form1 : Form
+    {
+        OracleConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string conStr = @"DATA SOURCE = localhost:1521/orcl; USER ID=marmijo;PASSWORD=marmijo";
+            conn = new OracleConnection(conStr);
+            updateGried();
+        }
+
+        private void updateGried()
+        {
+            conn.Open();
+            OracleCommand getemps= conn.CreateCommand();
+            getemps.CommandText = "SELECT * FROM DATOS";
+            getemps.CommandType = CommandType.Text;
+            OracleDataReader empDR= getemps.ExecuteReader();
+            DataTable empdt = new DataTable();
+            empdt.Load(empDR);
+            dataGridView1.DataSource=empdt;
+            conn.Close();
+        }
+
+    }
+}
--- WinAppBiblioteca/Program.cs
+++ WinAppBiblioteca/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinAppBiblioteca.Forms;
 
 namespace WinAppBiblioteca
 {
@@ -16,7 +17,7 @@ namespace WinAppBiblioteca
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Diseno());
+            Application.Run(new Form1());
         }
     }
 }
