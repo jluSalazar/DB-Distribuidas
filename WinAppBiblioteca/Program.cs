19101c33b13cde9839f79695dd0cc419952d06cb
Selenoseven <120606545+Selenoseven@users.noreply.github.com>
2023-08-31 18:30:00 -0500

    Update Program.cs

--- WinAppBiblioteca/Program.cs
+++ WinAppBiblioteca/Program.cs
@@ -18,7 +18,7 @@ namespace WinAppBiblioteca
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new ProveedorGYEinsert(false));
+            Application.Run(new VistaMaterializada());
 
         }
     }
